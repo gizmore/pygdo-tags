e2c2a10eb57ebc61b5d9d71e551a4ae1ebbe87a2
gizmore <gizmore@wechall.net>
2025-12-09 18:47:24 +0100

    'tree'

--- GDO_Tags.py
+++ GDO_Tags.py
@@ -1,13 +1,17 @@
+from gdo.base.Exceptions import GDOError
 from gdo.base.GDO import GDO
 from gdo.base.GDT import GDT
+from gdo.core.GDT_AutoInc import GDT_AutoInc
+from gdo.core.GDT_Name import GDT_Name
 
 
 class GDO_Tags(GDO):
 
     def gdo_tagged_object_table(self) -> GDO:
-        raise GDOE
+        raise GDOError("gdo_tagged_object_table: Not implemented for "+self.fqcn())
 
     def gdo_columns(self) -> list[GDT]:
         return [
-
+            GDT_AutoInc('tag_id'),
+            GDT_Name('tag_name'),
         ]
