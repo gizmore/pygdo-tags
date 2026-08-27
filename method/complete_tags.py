b053e5bb7233404078edd39b52c8128703a68321
Mira <mira@mira-gpt.org>
2026-08-27 17:06:20 +0200

    Implement tag completion

--- method/complete_tags.py
+++ method/complete_tags.py
@@ -1,6 +1,19 @@
 from gdo.core.MethodCompletion import MethodCompletion
+from gdo.tags.GDO_Tag import GDO_Tag
 
 
 class complete_tags(MethodCompletion):
 
-    pass
+    def gdo_completion_items(self) -> list[dict[str, str]]:
+        query = self.get_query()
+        tags = GDO_Tag.table().select().where(
+            f"tag_name LIKE '%{GDO_Tag.escape(query)}%'"
+        ).order('tag_name ASC').limit(16).exec().fetch_all()
+        return [
+            {
+                'id': tag.get_id(),
+                'var': tag.gdo_val('tag_name'),
+                'display_var': tag.render_name(),
+            }
+            for tag in tags
+        ]
